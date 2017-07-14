74202bcb7e311cdb6b99f3cd2d8dcdce1ebc481b
Emil Karimov <emvakar@gmail.com>
2017-07-14 19:33:43 +0300

    change api to alamofire

--- code name Russia/APIs/APIF.swift
+++ code name Russia/APIs/APIF.swift
@@ -7,14 +7,40 @@
 //
 
 import Foundation
+import Alamofire
 
 
 class APIFour {
     static let shared = APIFour()
     
+    let token = "EKIVXT5WDTZU1GXL45WSFW5EG3UCG2KW5RHEWFJKZCWDQWMN&v=20170714"
+    let url   = URL(string: "https://api.foursquare.com/v2/venues/explore")!
     
     func getNearbyPLaces() {
         
+        let parameters: Parameters = ["ll": "40.7,-74",
+                          "llAcc": 500.0,
+                          "altAcc": 500.0,
+                          "oauth_token": token,
+                          "v": "20170714",]
+        Alamofire.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseJSON { (responseJSON) in
+            if responseJSON.result.isSuccess {
+                do {
+                    if let responseJSON = try JSONSerialization.jsonObject(with: responseJSON.data!, options: []) as? [String: Any] {
+                        print(responseJSON)
+//                        let plases = Place {
+//                        completionHandler(todo, nil)
+                    } else {
+                        
+                    }
+                } catch {
+                    return
+                }
+            } else {
+                print(responseJSON.error?.localizedDescription ?? "")
+                
+            }
+        }
         
         
         
