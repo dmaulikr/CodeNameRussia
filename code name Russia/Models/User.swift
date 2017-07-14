65538a7d0c78434c3180fb4bf2ae987a0e69dccf
Emil Karimov <emvakar@gmail.com>
2017-07-14 17:33:51 +0300

    first upload clean project

---
+++ code name Russia/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  code name Russia
+//
+//  Created by Emil Karimov on 14.07.17.
+//  Copyright © 2017 Emil Karimov. All rights reserved.
+//
+
+import Foundation
+
+
+class User: Decodable {
+    let userId: Int!
+    var liginName: String!
+    
+    
+}
